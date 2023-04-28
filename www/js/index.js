c5e1b4185740886c0bb71f4fd13a1b172536dbb3
prabin <kabu.s.prabin@gmail.com>
2023-04-28 08:44:06 +0530

    28.04.2023

--- www/js/index.js
+++ www/js/index.js
@@ -24,6 +24,4 @@ document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
 }
